89f0d36e835ddcd2703dbabcaf99473082183361
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-15 13:42:07 +0100

    API call in app js for event and extract using useLoader hook.

--- frontend/src/pages/Event.jsx
+++ frontend/src/pages/Event.jsx
@@ -1,17 +1,15 @@
-import {NavLink} from "react-router-dom";
+import { useLoaderData } from 'react-router-dom';
 
-const DummyData = [
-  { id: "e1", title: "Event One" },
-  { id: "e2", title: "Event Two" },
-];
-export default function Event() {
+import EventsList from '../components/EventList';
+
+function EventsPage() {
+  const events = useLoaderData(); // useLoaderData() reads by react and automatically extracts data from promise which comes from async api call from app.js file.
+  // Also useLoaderData() can be use to get data in same element that assinged to root AND in call comopnents that are inside of that element. like in EventList
   return (
-    <ul>
-      {DummyData.map((event) => {
-        return <li key={event.id}>
-          <NavLink to={event.id}>{event.title}</NavLink>
-          </li>;
-      })}
-    </ul>
+    <>
+      {<EventsList events={events} />}
+    </>
   );
 }
+
+export default EventsPage;
