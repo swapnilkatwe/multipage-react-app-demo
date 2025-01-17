19eb92c6e7b9afa83d4f1f74fc56ebe0f5845331
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-17 12:14:04 +0100

    Update for async api call and rendering page with showing other components and load async data afterwords. Using Await from "react-router-dom" and  Suspense  from "react";

--- frontend/src/pages/Event.jsx
+++ frontend/src/pages/Event.jsx
@@ -1,17 +1,29 @@
-import { useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { useLoaderData, Await } from "react-router-dom";
 
 import EventsList from "../components/EventList";
 
 function EventsPage() {
-  const data = useLoaderData(); // useLoaderData() reads by react and automatically extracts data from promise which comes from async api call from app.js file.
-  // Also useLoaderData() can be use to get data in same element that assinged to root AND in call comopnents that are inside of that element. like in EventList
-  const events = data.events;
-  return <>{<EventsList events={events} />}</>;
+  // -->NOTE 1: Code for async api call and waiting to get data from async api call and render page after that.
+  // const data = useLoaderData(); // useLoaderData() reads by react and automatically extracts data from promise which comes from async api call from app.js file.
+  // // Also useLoaderData() can be use to get data in same element that assinged to root AND in call comopnents that are inside of that element. like in EventList
+  // const events = data.events;
+  // return <EventsList events={events} />
+
+  // -->NOTE 2: Code for async api call and rendering page with showing other components and load async data afterwords.
+  const { events } = useLoaderData();
+  return (
+    <Suspense fallback={<p style={{textAlign: "center"}}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     throw new Response(
@@ -27,7 +39,16 @@ export async function loader() {
     );
     //...
   } else {
+    // -->NOTE 2
+    const resData = await response.json();
+    // --> NOTE 1
     // const resData = await response.json();
-    return response; // useLoaderData automatically will extract data from the response
+    // return response; // useLoaderData automatically will extract data from the response
+    return resData.events;
   }
+}
+export function loader() {
+  return {
+    events: loadEvents(), // events must return a promise and loadEvents returning the promise as async function.
+  };
 }
