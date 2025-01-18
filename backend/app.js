49d510e40ece6dc1d7b2729e34ccf0e7b77057da
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-18 18:38:23 +0100

    backend: Installed jsonwebtoken and bcryptjs libraries. User Authentication code added.

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 
 const eventRoutes = require('./routes/events');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -12,7 +13,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
-
+app.use(authRoutes);
 app.use('/events', eventRoutes);
 
 app.use((error, req, res, next) => {
