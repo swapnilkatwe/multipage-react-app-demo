d6b80cc192fdf42bca61c359a225387b731530a1
Swapnil Katwe <swapnil.katwe111@gmail.com>
2025-01-19 17:30:37 +0100

    signup or signin functionality action added in authentication jsx file.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,9 @@ import RootLayout from "./pages/RootLayout";
 import ErrorDetail from "./components/ErrorDetail";
 import EventsRoot from "./pages/EventsRoot";
 import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
-import AuthenticationPage from "./pages/Authentication";
+import AuthenticationPage, {
+  action as authAction,
+} from "./pages/Authentication";
 
 function App() {
   const router = createBrowserRouter([
@@ -23,7 +25,7 @@ function App() {
       children: [
         // With Relative paths
         { index: true, element: <Home /> },
-        { path: "auth", element: <AuthenticationPage /> },
+        { path: "auth", element: <AuthenticationPage />, action: authAction },
         {
           path: "events",
           element: <EventsRoot />,
--- frontend/src/pages/Authentication.jsx
+++ frontend/src/pages/Authentication.jsx
@@ -1,7 +1,55 @@
-import AuthForm from '../components/AuthForm';
+import { redirect } from "react-router-dom";
+import AuthForm from "../components/AuthForm";
 
 function AuthenticationPage() {
   return <AuthForm />;
 }
 
 export default AuthenticationPage;
+
+export async function action({request}) {
+        
+  const searchParams = new URL(request.url).searchParams;
+console.log("action2");
+  const mode = searchParams.get("mode") || "login";
+  console.log("action"+ searchParams+ mode);
+
+  if (mode !== "login" && mode !== "signup") {
+    throw new Response(
+      JSON.stringify({ message: "Unsupported Mode" }, { status: 422 })
+    );
+  }
+
+  const formData = await request.formData();
+  const authData = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+
+  const response = await fetch("http://localhost:8080/" + mode, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(authData),
+  });
+
+  console.log("response: "+ JSON.stringify(response));
+  
+  if (response.status === 422 || response.status === 401) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw new Response(JSON.stringify({
+        message: "Couldnot authenticate user"
+    },{
+        status: 500
+    }));
+  }
+
+  // manage token pending
+
+  return redirect("/"); // redirect to Starting page
+
+}
